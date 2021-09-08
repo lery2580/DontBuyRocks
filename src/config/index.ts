88ccf256a81e9cd3846df3b233f1f3a05dbd550d
Lery <73690399+lery2580@users.noreply.github.com>
2021-09-08 17:50:14 +0800

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export default {
   // nftAddress: "0x0a78f2e0471Ab816383a25feBa5C35BE2C58122F",
-  nftAddress: "0x24a9772014090b29262e28e0f652f5e7a57a83b7",
+  nftAddress: "0x00d3499d3788ecf9f10cf8312d0db57c781702d5",
   preLink: "/DontBuyRocks/",
 };
