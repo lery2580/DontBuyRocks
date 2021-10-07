3418922bb9107b0008bf2fc756f9b861c9ad587d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-07 11:51:54 +0000

    Deploy lery2580/DontBuyRocks to github.com/lery2580/DontBuyRocks.git:gh-pages

---
+++ precache-manifest.416269da1fabdc46918c8011fcea991f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8885f0601e5d384154b00f0b9c009502",
+    "url": "./index.html"
+  },
+  {
+    "revision": "acc41ed11d1bfc6dbe01",
+    "url": "./static/css/1.88f4848e.chunk.css"
+  },
+  {
+    "revision": "52aeef4b7a0892be76c9",
+    "url": "./static/css/5.feaef2cf.chunk.css"
+  },
+  {
+    "revision": "75758dd8f39b245ee24b",
+    "url": "./static/css/6.f054bd84.chunk.css"
+  },
+  {
+    "revision": "81d23aaf8392c77225d7",
+    "url": "./static/css/7.23de7fb6.chunk.css"
+  },
+  {
+    "revision": "e1172f4f9e994c4cb05a",
+    "url": "./static/css/main.2d2b530e.chunk.css"
+  },
+  {
+    "revision": "88976d361f8e7fee4436",
+    "url": "./static/js/0.0114648f.chunk.js"
+  },
+  {
+    "revision": "d02ec6d2778552254da8249efee038aa",
+    "url": "./static/js/0.0114648f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acc41ed11d1bfc6dbe01",
+    "url": "./static/js/1.ab78bb3a.chunk.js"
+  },
+  {
+    "revision": "cfd2d2bdd16e7d00507e",
+    "url": "./static/js/4.cc96bc0d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/4.cc96bc0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52aeef4b7a0892be76c9",
+    "url": "./static/js/5.2929da62.chunk.js"
+  },
+  {
+    "revision": "75758dd8f39b245ee24b",
+    "url": "./static/js/6.af94006c.chunk.js"
+  },
+  {
+    "revision": "81d23aaf8392c77225d7",
+    "url": "./static/js/7.38436386.chunk.js"
+  },
+  {
+    "revision": "e1172f4f9e994c4cb05a",
+    "url": "./static/js/main.aa2f8931.chunk.js"
+  },
+  {
+    "revision": "df330cb326b6e0d4f4a1",
+    "url": "./static/js/runtime-main.61668eb1.js"
+  },
+  {
+    "revision": "f33bf15d4e137c4f4333ba542e9c8a5c",
+    "url": "./static/media/0.f33bf15d.png"
+  },
+  {
+    "revision": "ce84f9fe897e101e979f227944e82c68",
+    "url": "./static/media/1.ce84f9fe.png"
+  },
+  {
+    "revision": "1ad9184bfcdd232908971225b6802add",
+    "url": "./static/media/2.1ad9184b.png"
+  },
+  {
+    "revision": "6b31d6ecf51e1b5fa32ddbb346802d08",
+    "url": "./static/media/3.6b31d6ec.png"
+  },
+  {
+    "revision": "6ddcb2fd6ee4817c7b981c3af3499c84",
+    "url": "./static/media/4.6ddcb2fd.gif"
+  },
+  {
+    "revision": "67482643623db5c0f901033c49ca6b1d",
+    "url": "./static/media/5.67482643.png"
+  },
+  {
+    "revision": "4d72f9c0e34f201e65a9e285d384a123",
+    "url": "./static/media/6.4d72f9c0.png"
+  },
+  {
+    "revision": "8f4c2a55aa3007a3c43961d17096f6cc",
+    "url": "./static/media/7.8f4c2a55.png"
+  },
+  {
+    "revision": "f8c2a0872307e2363cb6602efb5d6e2f",
+    "url": "./static/media/8.f8c2a087.png"
+  },
+  {
+    "revision": "b48569238eeb0d62de28a5d056b16bef",
+    "url": "./static/media/Gabriela-Regular.b4856923.ttf"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "./static/media/Lobster-Regular.9406d0ab.ttf"
+  },
+  {
+    "revision": "2089f31a66e5fcb9197c3e3b5152822a",
+    "url": "./static/media/bg01.2089f31a.png"
+  }
+]);
